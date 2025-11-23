533987fd229ab100970e044727af347344c5950f
Doretolisa <monalisadoreto.a@gmail.com>
2025-11-22 23:15:25 -0300

    :construction:

---
+++ dados.js
@@ -0,0 +1,125 @@
+const cursos = [
+    {
+        id: 1,
+        nome: "M-tec Administração",
+        descricao: "Curso voltado para gestão, organização e processos administrativos.",
+        duracao: "1 ano e 6 meses",
+        img: "https://i.pinimg.com/474x/7a/24/d6/7a24d6d6d9b25efb7a35741fc47acfb1.jpg",
+        conteudo: [
+            "Ensino Médio Integral (PI) ou Modular (Vespertino)",
+            "Gestão empresarial",
+            "Processos administrativos",
+            "Contabilidade básica",
+            "Direito do trabalho",
+            "Marketing",
+            "Projeto final (TCC)"
+        ]
+    },
+    {
+        id: 2,
+        nome: "M-tec Informática Para Internet",
+        descricao: "Curso focado em programação web, design e tecnologias da internet.",
+        duracao: "1 ano e 6 meses",
+        img: "https://i.pinimg.com/236x/57/ea/89/57ea898844b18668b14be3a189928999.jpg",
+        conteudo: [
+            "Ensino Médio Integral (PI) ou Modular (Vespertino)",
+            "Programação e desenvolvimento web",
+            "Banco de dados",
+            "Desenvolvimento mobile",
+            "Redes e segurança",
+            "Design, usabilidade e acessibilidade",
+            "Marketing digital e gestão de conteúdo",
+            "Projeto final (TCC)"
+        ]
+    },
+    {
+        id: 3,
+        nome: "M-tec Linguagens, Ciências Humanas e Sociais Aplicadas",
+        descricao: "Curso interdisciplinar voltado para comunicação e ciências humanas.",
+        duracao: "1 ano e 6 meses",
+        img: "https://hideaway.com.ua/images/categories/basephoto.jpg",
+        conteudo: [
+            "Ensino Médio Integral (PI) ou Modular (Vespertino)",
+            "Comunicação oral e escrita",
+            "Linguagens artísticas",
+            "Ciências humanas",
+            "Sociologia e filosofia",
+            "Projeto final (TCC)"
+        ]
+    },
+    {
+        id: 4,
+        nome: "M-tec Mecatrônica",
+        descricao: "Curso que integra mecânica, eletrônica e automação industrial.",
+        duracao: "2 anos",
+        img: "https://i.pinimg.com/originals/4d/08/fb/4d08fb74db5ff9e4dc28f6e13352a3d9.jpg",
+        conteudo: [
+            "Ensino Médio Integral (PI) ou Modular (Vespertino)",
+            "Mecânica básica",
+            "Eletrônica aplicada",
+            "Automação industrial",
+            "Robótica",
+            "Projeto final (TCC)"
+        ]
+    },
+    {
+        id: 5,
+        nome: "M-tec Meio Ambiente",
+        descricao: "Curso focado em ecologia, preservação e sustentabilidade ambiental.",
+        duracao: "1 ano e 6 meses",
+        img: "https://www.fetranspar.org.br/admarq/noticias/not297685.jpg",
+        conteudo: [
+            "Ensino Médio Integral (PI) ou Modular (Vespertino)",
+            "Ecologia e biodiversidade",
+            "Preservação ambiental",
+            "Gestão de resíduos",
+            "Sustentabilidade",
+            "Projeto final (TCC)"
+        ]
+    },
+    {
+        id: 6,
+        nome: "M-tec Química",
+        descricao: "Curso voltado para análises químicas, laboratório e processos industriais.",
+        duracao: "2 anos",
+        img: "https://i.pinimg.com/236x/f0/d6/20/f0d6205a78e53e8e8a248da4a503ee87.jpg",
+        conteudo: [
+            "Ensino Médio Integral (PI) ou Modular (Vespertino)",
+            "Química geral e analítica",
+            "Técnicas laboratoriais",
+            "Processos industriais",
+            "Segurança no trabalho",
+            "Projeto final (TCC)"
+        ]
+    },
+    {
+        id: 7,
+        nome: "M-tec Logística",
+        descricao: "Curso sobre transporte, armazenagem, distribuição e cadeia de suprimentos.",
+        duracao: "1 ano e 6 meses",
+        img: "https://img.freepik.com/fotos-premium/patrao-e-empregado-parceria-bem-sucedida-duas-mulheres-brainstorming-em-colaboracao-de-escritorio-e-conceito-de-formacao-de-equipe-empresaria-e-assistente-de-secretaria-olhar-documentacao_474717-127601.jpg",
+        conteudo: [
+            "Ensino Médio Integral (PI) ou Modular (Vespertino)",
+            "Transporte e armazenagem",
+            "Distribuição e cadeia de suprimentos",
+            "Gestão de estoques",
+            "Planejamento logístico",
+            "Projeto final (TCC)"
+        ]
+    },
+    {
+        id: 8,
+        nome: "M-tec Eletrônica",
+        descricao: "Curso sobre circuitos, dispositivos eletrônicos e sistemas embarcados.",
+        duracao: "2 anos",
+        img: "https://guiadoestudante.abril.com.br/wp-content/uploads/sites/4/2018/06/engenharia-eletrica-2.jpg?quality=70&strip=info&w=414&h=280&crop=1",
+        conteudo: [
+            "Ensino Médio Integral (PI) ou Modular (Vespertino)",
+            "Circuitos elétricos",
+            "Dispositivos eletrônicos",
+            "Sistemas embarcados",
+            "Manutenção eletrônica",
+            "Projeto final (TCC)"
+        ]
+    }
+];
---
+++ trabalhos.js
@@ -0,0 +1,37 @@
+const trabalhos = [
+    {
+        id: 1,
+        titulo: "Projeto de Robótica",
+        imagem: "https://example.com/banner1.jpg",
+        legenda: "Construção realizada pelos alunos do 3° ano de Mecatrônica",
+        descricao: "Projeto envolvendo servo motores, sensores e Arduino em um braço robótico funcional.",
+        link: "https://www.youtube.com/watch?v=xxxxx"
+    },
+
+    {
+        id: 2,
+        titulo: "Experimento Químico - Reação Cromática",
+        imagem: "https://example.com/foto2.jpg",
+        legenda: "Experimento feito no laboratório utilizando reagentes de mudança de cor.",
+        descricao: "Os alunos do curso de Química realizaram um experimento demonstrando reações redox e efeitos catalíticos.",
+        link: "https://google.com"
+    },
+
+    {
+        id: 3,
+        titulo: "Website - Informática para Internet",
+        imagem: "https://example.com/foto3.jpg",
+        legenda: "Primeiro projeto completo de HTML, CSS e JavaScript dos alunos.",
+        descricao: "Um website institucional desenvolvido pelos alunos do 2° módulo com foco em responsividade.",
+        link: "https://github.com/exemplo"
+    },
+
+    {
+        id: 4,
+        titulo: "Projeto de Sustentabilidade",
+        imagem: "https://example.com/foto4.jpg",
+        legenda: "Trabalho interdisciplinar envolvendo Meio Ambiente e Logística.",
+        descricao: "Projeto que analisa o impacto de resíduos sólidos e propõe soluções sustentáveis para a escola.",
+        link: "https://example.com/projeto-sustentavel"
+    }
+];
